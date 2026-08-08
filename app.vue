d061165dac1c8a9dcb82e5ea9a26497c523a72b8
Serhii Podvysotskyi <serhii@podvysotskyi.cc>
2026-08-08 14:41:20 -0500

    feat: streamline local development workflow

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import { useHead } from '#imports'
+import faviconUrl from '~/assets/favicon.ico'
+
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      href: faviconUrl,
+      type: 'image/x-icon',
+      sizes: '16x16 32x32'
+    }
+  ]
+})
+</script>
+
 <template>
   <UApp>
     <NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,6 @@ import { fileURLToPath } from 'node:url'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'icon',
-          href: '/favicon.ico',
-          type: 'image/x-icon',
-          sizes: '16x16 32x32'
-        }
-      ]
-    }
-  },
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   icon: {
@@ -23,7 +11,9 @@ export default defineNuxtConfig({
   },
   modules: ['@nuxt/ui', '@pinia/nuxt'],
   dir: {
-    public: fileURLToPath(new URL('../../assets', import.meta.url))
+    public:
+      process.env.L2_PUBLIC_ASSETS_DIR ??
+      fileURLToPath(new URL('../../assets', import.meta.url))
   },
   runtimeConfig: {
     public: {
